e1939b0217f9fda9c24d76157a289f4c58ed451c
tim wu <twu@terracottatech.com>
2015-04-15 20:37:10 +0000

    use some default amount of offheap (and hope for the best)
    svn path=/trunk/ehcache/; revision=9733

--- system-tests/src/test/java/org/terracotta/ehcache/tests/AbstractCacheTestBase.java
+++ system-tests/src/test/java/org/terracotta/ehcache/tests/AbstractCacheTestBase.java
@@ -44,6 +44,8 @@ public class AbstractCacheTestBase extends AbstractTestBase {
                                Class<? extends AbstractClientBase>... c) {
     super(testConfig);
     this.ehcacheConfigPath = ehcacheConfigPath;
+    testConfig.getL2Config().setOffHeapEnabled(true);
+    testConfig.getL2Config().setMaxOffHeapDataSize(512);
     testConfig.getL2Config().setMaxHeap(1024);
     testConfig.getL2Config().setMinHeap(256);
     testConfig.getClientConfig().addExtraClientJvmArg("-Dnet.sf.ehcache.skipUpdateCheck=true");
