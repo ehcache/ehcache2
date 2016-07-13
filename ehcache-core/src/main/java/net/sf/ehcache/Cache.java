1bb9826f7dd960a903404d43644cbbe7d8e08da5
Jacomet Louis <Louis.Jacomet@terracottatech.com>
2016-07-13 15:13:04 +0000

    EHC-1132 merge branches/ehcache-2.7.x@10397
    svn path=/branches/ehcache-2.8.x/; revision=10398

--- ehcache-core/src/main/java/net/sf/ehcache/Cache.java
+++ ehcache-core/src/main/java/net/sf/ehcache/Cache.java
@@ -1809,8 +1809,6 @@ public class Cache implements InternalEhcache, StoreListener {
      * If the Element is not in the cache, the associated cache loader will be called. That is either the CacheLoader passed in, or if null,
      * the one associated with the cache. If both are null, no load is performed and null is returned.
      * <p/>
-     * If the loader decides to assign a null value to the Element, an Element with a null value is created and stored in the cache.
-     * <p/>
      * Because this method may take a long time to complete, it is not synchronized. The underlying cache operations
      * are synchronized.
      *
@@ -1818,7 +1816,8 @@ public class Cache implements InternalEhcache, StoreListener {
      * @param loader         the override loader to use. If null, the cache's default loader will be used
      * @param loaderArgument an argument to pass to the CacheLoader.
      * @return an element if it existed or could be loaded, otherwise null
-     * @throws CacheException
+     *
+     * @throws CacheException if the loading fails
      */
     public Element getWithLoader(Object key, CacheLoader loader, Object loaderArgument) throws CacheException {
 
@@ -1847,15 +1846,23 @@ public class Cache implements InternalEhcache, StoreListener {
             } else {
                 value = loadValueUsingLoader(key, loader, loaderArgument);
             }
-            if (value != null) {
-                put(new Element(key, value), false);
+            if (value == null) {
+                return getQuiet(key);
+            } else {
+                Element newElement = new Element(key, value);
+                put(newElement, false);
+                Element fromCache = getQuiet(key);
+                if (fromCache == null) {
+                    return newElement;
+                } else {
+                    return fromCache;
+                }
             }
         } catch (TimeoutException e) {
             throw new LoaderTimeoutException("Timeout on load for key " + key, e);
         } catch (Exception e) {
             throw new CacheException("Exception on load for key " + key, e);
         }
-        return getQuiet(key);
     }
 
     /**
